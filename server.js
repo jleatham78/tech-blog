3d9544cb8a7b7b6c8a70aa8e3caf1117c5a4b75a
jleatham78 <janeleleatham01@gmail.com>
2021-02-21 12:46:24 -0700

    used env

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'mycatisfat',
+  secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
